20a5496dcae8ae176b116565416b685e831ba8a8
Patrick Kelso <psk@puppetlabs.com>
2015-02-27 16:18:12 +1100

    Placeholder commit until I get home and fix this

--- manifests/shm.pp
+++ manifests/shm.pp
@@ -13,6 +13,14 @@ class scaleio::shm (
 	    match => "^tmpfs",
 	    line => "tmpfs  /dev/shm  tmpfs defaults,size=${shm_size}  0 0",
 	} ->
+# The code below is failing for me using the Vagrant Box. Need to investigate.
+# Puppet Open Source 3.7.4.
+# Error: Could not retrieve catalog from remote server: Error 400 on SERVER: Evaluation Error: 
+# Comparison of: String < Integer, is not possible. Caused by 'A String is not comparable to a 
+# non String'. at /etc/puppet/modules/scaleio/manifests/shm.pp:16:22 on node tb.scaleio.local
+
+# If I comment it out Everything works.
+
 	if ($::kernelshmmax < 209715200) {
 	    exec {'set kernel shmmax' :
 	      command   => 'sysctl -p 209715200',
